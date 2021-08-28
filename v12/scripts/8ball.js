7a0665dbb800a79723032018d2c379710f066bae
Jeadness <itsbeadj@gmail.com>
2021-08-28 19:23:29 +0300

    Create 8ball.js

---
+++ v12/scripts/8ball.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require('discord.js');
+const prefix = "your-prefix-here"; // const { prefix } = require('your prefix location')
+
+module.exports = {
+    name: "8ball",
+    description: "enter description here",
+    category: "fun",
+    execute(client, message, args) {
+        let question = message.content.slice(prefix.length + 6);
+        if (!question)
+            return message.channel.send(`**<@${message.author.id}>, please ask a question.**`)
+            else {
+                let responses = [
+                    "sure",
+                    "no",
+                    "yes"
+                ];
+                let response =
+                responses[Math.floor(Math.random() * responses.length)];
+                let embed = new MessageEmbed()
+                    .setTitle('')
+                    .setDescription('')
+                    .setColor('RANDOM')
+                    .setTimestamp();
+                    message.channel.send(embed);
+            }
+    },
+};
