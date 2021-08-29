9efec03205816efe8def6f1274352d6ac6dab187
Jeadness <itsbeadj@gmail.com>
2021-08-29 13:41:08 +0300

    add kick script

---
+++ v13/scripts/kick.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Kicks a member.')
+		.addUserOption(option => option.setName('target').setDescription('The member to kick')),
+	async execute(interaction) {
+		const member = interaction.options.getMember('target');
+		member.kick();
+		return interaction.reply({ content: `You kicked: ${member.user}`, ephemeral: true });
+	},
+};
