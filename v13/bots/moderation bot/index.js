da25fa0f93ba69d1293fce434cc570269dda4d4a
Jeadness <itsbeadj@gmail.com>
2021-08-29 09:20:34 +0300

    Update index.js

--- v13/bots/moderation bot/index.js
+++ v13/bots/moderation bot/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config/token.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { client_id } = require('./config/client_id.json');
+const { guild_id } = require('./config/guild_id.json');
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -16,3 +18,5 @@ client.once('ready', () => {
     console.log(on_ready_message)
     console.log(`Logged in as ${client.user}`)
 });
+
+client.login(token);
