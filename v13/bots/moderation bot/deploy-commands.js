a8105279bed3398553efbbe3cd190452b503cf4f
Jeadness <itsbeadj@gmail.com>
2021-08-29 11:28:57 +0300

    update moderation bot ;)

---
+++ v13/bots/moderation bot/deploy-commands.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config/token.json');
+const { client_id } = require('./config/client_id.json');
+const { guild_id } = require('./config/guild_id.json');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		await rest.put(
+			Routes.applicationGuildCommands(client_id, guild_id),
+			{ body: commands },
+		);
+
+		console.log('Successfully registered application commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
--- v13/bots/moderation bot/index.js
+++ v13/bots/moderation bot/index.js
@@ -9,14 +9,29 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
 }
 
 client.once('ready', () => {
     let on_ready_message = require('./config/on_ready_message.json');
     console.log(on_ready_message)
-    console.log(`Logged in as ${client.user}`)
+    console.log(`Logged in as ${client.user.tag}`)
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 client.login(token);
