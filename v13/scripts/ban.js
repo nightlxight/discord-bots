3285d46382106c7ba46715b4dc925dbb4bf2b460
Jeadness <itsbeadj@gmail.com>
2021-08-30 10:57:58 +0300

    add ban command

---
+++ v13/scripts/ban.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ban')
+		.setDescription('Bans a member.')
+		.addUserOption(option => option.setName('target').setDescription('The member to ban')),
+	async execute(interaction) {
+		const user = interaction.options.getUser('target');
+		guild.members.ban(user);
+		return interaction.reply({ content: `You banned: ${user.tag}`, ephemeral: true });
+	},
+};
+
+// MAKE SURE YOU RUN THE DEPLOY SCRIPT EVERYTIME YOU ADD A COMMAND
+// For example, node deploy-commands.js
