a084da93f1f4290ed1dde0b20b8f35ae12445ba6
Jeadness <itsbeadj@gmail.com>
2021-08-30 11:06:05 +0300

    add unban command to scripts v13, any help would be appreciated :)

---
+++ v13/scripts/unban.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('unban')
+		.setDescription('Unbans a member.')
+		.addUserOption(option => option.setName('target').setDescription('The member to unban')),
+	async execute(interaction) {
+		const user = interaction.options.getUser('target');
+        const id = interaction.options.get('target')?.value;
+		guild.members.unban(id);
+		return interaction.reply({ content: `You unbanned: ${user.tag}`, ephemeral: true });
+	},
+};
+
+// MAKE SURE YOU RUN THE DEPLOY SCRIPT EVERYTIME YOU ADD A COMMAND
+// For example, node deploy-commands.js
