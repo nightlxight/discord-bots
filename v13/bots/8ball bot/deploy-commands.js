480573fd17ba13da03a3d26b7a27c8457991531d
Jeadness <itsbeadj@gmail.com>
2021-08-31 09:54:30 +0300

    update 8ball bot | any help would be appreciated :)

---
+++ v13/bots/8ball bot/deploy-commands.js
@@ -0,0 +1,9 @@
+/* 
+Coming soon!
+Any help would be appreciated!
+*/
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./data/token.json');
+const { client_id } = require('./data/client_id.json');
--- v13/bots/8ball bot/index.js
+++ v13/bots/8ball bot/index.js
@@ -1,5 +1,17 @@
-/*
-coming soon 
+/* 
+Coming soon!
+Any help would be appreciated!
 */
 
 const fs = require('fs');
+const { token } = require('./data/token.json');
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`)
+});
+
+
+client.login(token);
