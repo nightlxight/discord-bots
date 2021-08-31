7bed7e73bc2b25467c168e8ecbee0509d589d1ca
Jeadness <itsbeadj@gmail.com>
2021-08-31 15:24:17 +0300

    update 8ball bot for v13

--- v13/bots/8ball bot/deploy-commands.js
+++ v13/bots/8ball bot/deploy-commands.js
@@ -5,5 +5,27 @@ Any help would be appreciated!
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { token } = require('./data/token.json');
-const { client_id } = require('./data/client_id.json');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successful!');
+	} catch (error) {
+		console.error(error);
+	}
+})();
