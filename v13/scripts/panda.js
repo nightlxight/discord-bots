877ba49e977d331e95dfed9195ab0decd427da4a
Jeadness <itsbeadj@gmail.com>
2021-09-01 14:41:02 +0300

    Create panda.js | any help would be appreciated, thanks :)

--- v13/scripts/panda.js
+++ v13/scripts/panda.js
@@ -1,24 +1,30 @@
+/*
+Requested by: fioragaming
+*/
+
+/*
+MAKE SURE YOU RUN THE DEPLOY SCRIPT EVERYTIME YOU ADD A COMMAND
+For example, node deploy-commands.js 
+*/
+const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { channel } = require('diagnostics_channel');
-const { execute } = require('./ban');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('panda')
-    .setDescription('Sends panda gif!'),
-    async execute(interaction) {
-        let gifs = [
-            "https://tenor.com/view/happy-panda-bamboo-panda-hungry-panda-eating-gif-17804693",
-            "https://tenor.com/view/panda-dancing-moves-funny-shaking-gif-17764808",
-            "https://tenor.com/view/panda-slide-cute-playing-gif-16649621",
-            "https://tenor.com/view/panda-gif-5303855",
-            "https://media.giphy.com/media/K9z3im98oo9Ve/giphy.gif?cid=ecf05e47lk20edsdf2dhxko7ddz3wr8nbovsf6bycaupggk6&rid=giphy.gif&ct=g",
-            "https://tenor.com/view/sunday-gif-21211301"
-            // "your gif link here",
-            // "the last gif should not contain a comma"
-        ];
-
-        let gif = gifs[Math.floor(Math.random() * gifs.length)];
-        channel.send({ embeds: []})
-    }
+        .setName('panda')
+        .setDescription('Sends panda gif!'),
+        async execute(interaction) {
+            let gifs = [
+                "https://tenor.com/view/happy-panda-bamboo-panda-hungry-panda-eating-gif-17804693",
+                "https://tenor.com/view/panda-dancing-moves-funny-shaking-gif-17764808",
+                "https://tenor.com/view/panda-slide-cute-playing-gif-16649621",
+                "https://tenor.com/view/panda-gif-5303855",
+                "https://media.giphy.com/media/K9z3im98oo9Ve/giphy.gif?cid=ecf05e47lk20edsdf2dhxko7ddz3wr8nbovsf6bycaupggk6&rid=giphy.gif&ct=g",
+                "https://tenor.com/view/sunday-gif-21211301" //,
+                // "your gif link here",
+                // "the last gif should not contain a comma"
+            ];
+            let gif = gifs[Math.floor(Math.random() * gifs.length)];
+            return interaction.reply(gif);
+        },
 };
