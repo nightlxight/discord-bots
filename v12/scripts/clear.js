e9f4392e6cf6cd2df21556e580eb4e72cd3a41ab
nightlxight <itsbeadj@gmail.com>
2021-09-04 17:36:40 +0300

    add clear script for v12 | any help would be appreciated

---
+++ v12/scripts/clear.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+module.exports = {
+  name: 'clear',
+  description: 'Clears messages.',
+  async execute(client, message, args) {
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+      const deleteCount = parseInt(args[0], 10);
+      const deleteMessage = `**${deleteCount}** adet mesajı sildim!`;
+
+      if (!deleteCount || deleteCount > 100 || deleteCount < 2) return message.reply('The number should be between 2 and 100.');
+      const fetched = await message.channel.messages.fetch({
+        limit: deleteCount
+      });
+      message.channel.bulkDelete(fetched)
+      .catch(err => message.channel.send(`Can't clear the messages because of: ${err}.`))
+      .then(message.reply(deleteMessage));
+    } else {
+      message.reply("You don't have permission to use this command!");
+    }
+  },
+};
