f338340c40c2973860b89a6a0c5b7c36c3fda6ce
nightlxight <itsbeadj@gmail.com>
2021-09-04 20:19:14 +0300

    add joke script for v12

---
+++ v12/scripts/joke.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+const { prefix } = require('../config.json');
+module.exports = {
+name: "joke",
+category: "fun",
+execute(client, message, args) {
+  let question = message.content.slice(prefix.length + 5);
+    let responses = [
+        "How do you stop an astronaut's baby from crying?\nYourocket!",
+        "How do you talk to a giant?\nUse big words!"
+        // add more jokes here!
+    ];
+    let response = responses[Math.floor(Math.random() * responses.length)];
+
+    let Embed = new MessageEmbed()
+      .setTitle('Joke command!')
+      .setDescription(`${response}`)
+      .setColor(`RANDOM`)
+      .setFooter(`Command executed by ${message.author.tag}`)
+      .setTimestamp();
+    message.channel.send(Embed);
+},
+};
