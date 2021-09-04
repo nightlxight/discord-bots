b1c5087d971569e01a4f70ce59c0687983a9d269
nightlxight <itsbeadj@gmail.com>
2021-09-04 18:44:06 +0300

    add catjam script for v13

---
+++ v13/scripts/catJAM.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('catjam')
+        .setDescription('Sends catJAM!'),
+    async execute(interaction) {
+        let gif = "https://media.tenor.com/images/7645a8d8641078195b89b1b7f096c7b2/tenor.gif";
+        return interaction.reply(gif);
+    },
+};
