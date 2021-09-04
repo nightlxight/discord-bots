3ddd46329f88fbbb7ac75735d68942e392627c30
nightlxight <itsbeadj@gmail.com>
2021-09-04 15:47:20 +0300

    add help and invite cmds to info bot v13
    any help would be appreciated

---
+++ v13/bots/information bot/commands/help.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { random_color, red, green, aqua, lightcyan, orange, yellow, purple, pink, cyan } = require('../colors.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Help command that shows list of commands'),
+    async execute(interaction) {
+        let embed = new MessageEmbed()
+            .setColor(random_color) // random_color, red, green, aqua, lightcyan, orange, yellow, purple, pink, cyan
+            .setTitle('Help')
+            .setDescription('Commands can be found below')
+            .addFields(
+                { name: `/invite`, value: 'Sends an invite link to bot!', inline: true},
+                { name: `/server`, value: 'Sends information about this server!', inline: true},
+            )
+            .setTimestamp()
+            .setFooter('Command executed by' + ` ${interaction.user.tag}`);
+        return interaction.reply(embed);
+    }
+}
