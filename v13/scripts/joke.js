c54729d354ad472c34ad8812fb82eec2f5939572
nightlxight <itsbeadj@gmail.com>
2021-09-04 20:45:37 +0300

    update joke command for v13

--- v13/scripts/joke.js
+++ v13/scripts/joke.js
@@ -5,3 +5,12 @@ any help would be appreciated
 
 var fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('joke')
+        .setDescription('Sends joke!'),
+    async execute(interaction) {
+        let jokes = [];
+    },
+};
