bc899929d55b471e84970aa76fb5fae1286f2dfe
nightlxight <itsbeadj@gmail.com>
2021-09-04 18:46:09 +0300

    add catJAM for v12

---
+++ v12/scripts/catJAM.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+module.exports = {
+	name: 'catjam',
+	description: 'Sends catJAM gif',
+	execute(client, message, args) {
+		let gifLink = "https://media.tenor.com/images/7645a8d8641078195b89b1b7f096c7b2/tenor.gif"
+        message.channel.send(gifLink);
+	},
+};
