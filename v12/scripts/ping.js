411b7723c1d745392f39309d9bd7c5ca42636d96
nightlxight <itsbeadj@gmail.com>
2021-09-05 12:38:52 +0300

    add ping script for v12

---
+++ v12/scripts/ping.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "ping",
+  category: "info",
+
+  description: "Sends API ping and latency ping",
+  timeout: 10000,
+	execute(client, message, args) {
+    message.channel.send(`🏓 Getting ping....`).then((msg) => {
+      const _ = new Discord.MessageEmbed()
+        .setTitle("Pong!")
+        .setDescription(
+          `🏓 Pong!\nLatency: ${Math.floor(
+            msg.createdTimestamp - message.createdTimestamp
+          )}ms\nAPI ping: ${Math.round(client.ws.ping)}ms`
+        )
+        .setColor("RANDOM");
+      msg.edit(_);
+      msg.edit("\u200B");
+    });
+  },
+};
