fe90449cf49765ab08b6791c3d79a5527203ceeb
nightlxight <itsbeadj@gmail.com>
2021-09-05 08:55:42 +0300

    Update invite.js

--- v12/scripts/invite.js
+++ v12/scripts/invite.js
@@ -5,3 +5,12 @@ coming soon
 */
 
 var fs = require('fs');
+const client_id = "Your bots application id here";
+
+module.exports = {
+    name: "invite",
+    description: "Sends an invite link to bot!",
+    execute(client, message, args) {
+        
+    },
+};
