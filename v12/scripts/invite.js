c5fefe0f4b4519322d80f9da010cded74de61cbc
nightlxight <itsbeadj@gmail.com>
2021-09-05 08:58:09 +0300

    Update invite.js

--- v12/scripts/invite.js
+++ v12/scripts/invite.js
@@ -11,6 +11,6 @@ module.exports = {
     name: "invite",
     description: "Sends an invite link to bot!",
     execute(client, message, args) {
-        
+        message.channel.send(`https://discord.com/api/oauth2/authorize?client_id=${client_id}&permissions=8&scope=bot%20applications.commands`)
     },
 };
