5ea1fd95be9fec3755de9e83ea93bb39d2254b9e
nightlxight <itsbeadj@gmail.com>
2021-09-05 09:10:02 +0300

    Update joke.js

--- v13/scripts/joke.js
+++ v13/scripts/joke.js
@@ -13,7 +13,11 @@ module.exports = {
         .setName('joke')
         .setDescription('Sends joke!'),
     async execute(interaction) {
-        let jokes = [];
+        let jokes = [
+            "How do you stop an astronaut's baby from crying?\nYourocket!",
+            "How do you talk to a giant?\nUse big words!" //,
+            // add more jokes here
+        ];
         let joke = jokes[Math.floor(Math.random() * jokes.length)];
     },
 };
