5bb2f7e4902bd4ccd3c68804e85e85bee045aa46
nightlxight <itsbeadj@gmail.com>
2021-09-06 18:16:59 +0300

    Update cat.js

--- v13/scripts/cat command/cat.js
+++ v13/scripts/cat command/cat.js
@@ -11,5 +11,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-
+    data: new SlashCommandBuilder()
+        .setName('cat')
+        .setDescription('')
 };
