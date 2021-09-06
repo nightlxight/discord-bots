0f151ee1ef0894611d552a938a1bc5d02f14c761
nightlxight <itsbeadj@gmail.com>
2021-09-06 18:31:31 +0300

    Update cat.js

--- v13/scripts/cat command/cat.js
+++ v13/scripts/cat command/cat.js
@@ -16,6 +16,8 @@ module.exports = {
         .setName('cat')
         .setDescription('Sends cat photo or gif!'),
     async execute(interaction) {
-        
+        let cat1 = gifs.cats.cat1;
+        let embed = new MessageEmbed()
+            .setColor('RANDOM')
     },
 };
