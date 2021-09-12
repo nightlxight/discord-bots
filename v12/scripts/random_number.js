b86d992eb7b9632c1a18a52851da64f0122a05c3
nightlxight <itsbeadj@gmail.com>
2021-09-12 11:06:12 +0300

    Update random_number.js

--- v12/scripts/random_number.js
+++ v12/scripts/random_number.js
@@ -4,5 +4,7 @@ module.exports = {
     name: "number",
     execute(client, message, args) {
         let number = Math.floor(Math.random() * 1000);
+        let embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
     }
 }
