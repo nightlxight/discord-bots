0da7f9780565d0e589245f557dc4bb0bbc6d792e
nightlxight <itsbeadj@gmail.com>
2021-09-12 14:06:46 +0300

    fix bug

--- v13/bots/information bot/index.js
+++ v13/bots/information bot/index.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
-const { Client, Collections, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { guild_id } = require('./data/guild_id.json');
 const { client_id } = require('./data/client_id.json');
 const { token } = require('./data/token.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-client.commands = new Collections();
+client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
