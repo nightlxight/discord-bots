854e600c1623e8dd7e78499bf50aa94b1cca0a81
nightlxight <itsbeadj@gmail.com>
2021-09-12 11:05:49 +0300

    Update random_number.js

--- v12/scripts/random_number.js
+++ v12/scripts/random_number.js
@@ -3,6 +3,6 @@ const Discord = require('discord.js');
 module.exports = {
     name: "number",
     execute(client, message, args) {
-        let number = Math.floor(Math.random() * 1000)
+        let number = Math.floor(Math.random() * 1000);
     }
 }
