b4f0858517bc1fdf78644650a4f9dff885330c91
nightlxight <itsbeadj@gmail.com>
2021-09-13 20:42:20 +0300

    Update index.js

--- v13/bots/8ball bot/index.js
+++ v13/bots/8ball bot/index.js
@@ -21,4 +21,16 @@ client.once('ready', () => {
     console.log();
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        return interaction.reply({ content: 'There was an error!', ephemeral: true });
+    }
+});
+
 client.login(token);
