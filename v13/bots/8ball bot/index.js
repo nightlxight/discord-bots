dbdb7a12afc753c9060d72d2577784c59ac16993
nightlxight <itsbeadj@gmail.com>
2021-09-13 20:40:10 +0300

    Update index.js

--- v13/bots/8ball bot/index.js
+++ v13/bots/8ball bot/index.js
@@ -12,3 +12,7 @@ const { client_id } = require('./data/client_id.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+}
