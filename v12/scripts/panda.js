53399997d62c88d29202432385f0bc7723261fcb
nightlxight <itsbeadj@gmail.com>
2021-09-13 20:24:50 +0300

    Update panda.js

--- v12/scripts/panda.js
+++ v12/scripts/panda.js
@@ -2,7 +2,7 @@
 Requested by: fioragaming
 */
 
-const fs = require('fs');
+var fs = require('fs');
 const Discord = require('discord.js');
 const config = require('config file location, for example ../config.json');
 module.exports = {
@@ -15,7 +15,7 @@ module.exports = {
             "https://tenor.com/view/panda-slide-cute-playing-gif-16649621",
             "https://tenor.com/view/panda-gif-5303855",
             "https://media.giphy.com/media/K9z3im98oo9Ve/giphy.gif?cid=ecf05e47lk20edsdf2dhxko7ddz3wr8nbovsf6bycaupggk6&rid=giphy.gif&ct=g",
-            "https://tenor.com/view/sunday-gif-21211301"
+            "https://tenor.com/view/sunday-gif-21211301" //,
             // "your gif link here",
             // "the last gif should not contain a comma"
         ];
