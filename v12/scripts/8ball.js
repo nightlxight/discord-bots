4dc505fa966078beac668b6c454bb5d0d120f269
nightlxight <itsbeadj@gmail.com>
2021-09-13 20:29:35 +0300

    Update 8ball.js

--- v12/scripts/8ball.js
+++ v12/scripts/8ball.js
@@ -24,6 +24,7 @@ module.exports = {
                     // last title should not contain a comma
                 ];
                 let title = titles[Math.floor(Math.random() * titles.length)];
+                
                 let embed = new MessageEmbed()
                     .setTitle('8 ball command!')
                     .setDescription(response)
