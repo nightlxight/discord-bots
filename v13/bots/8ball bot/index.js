8f72556ffe66932296dea6ffeff5ba7e40398900
nightlxight <itsbeadj@gmail.com>
2021-09-13 20:37:39 +0300

    Update index.js

--- v13/bots/8ball bot/index.js
+++ v13/bots/8ball bot/index.js
@@ -6,3 +6,6 @@ coming soon
 
 var fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./data/token.json');
+const { guild_id } = require('./data/guild_id.json');
+const { client_id } = require('./data/client_id.json');
