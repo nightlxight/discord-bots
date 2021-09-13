f742da5801fa9b46dc00ea63845a99b679486a6a
nightlxight <itsbeadj@gmail.com>
2021-09-13 20:38:52 +0300

    Update index.js

--- v13/bots/8ball bot/index.js
+++ v13/bots/8ball bot/index.js
@@ -9,3 +9,4 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./data/token.json');
 const { guild_id } = require('./data/guild_id.json');
 const { client_id } = require('./data/client_id.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
