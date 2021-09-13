71e5f63b404b1429045b1161e0b71ab06e975e82
nightlxight <itsbeadj@gmail.com>
2021-09-13 20:40:30 +0300

    Update index.js

--- v13/bots/8ball bot/index.js
+++ v13/bots/8ball bot/index.js
@@ -16,3 +16,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
 }
+
+client.once('ready', () => {
+    console.log();
+});
