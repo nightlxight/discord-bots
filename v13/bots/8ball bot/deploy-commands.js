a3fb04e2103dbcedb8a9c9e60269eed7e8fa1481
nightlxight <itsbeadj@gmail.com>
2021-09-14 16:06:48 +0300

    Update deploy-commands.js

--- v13/bots/8ball bot/deploy-commands.js
+++ v13/bots/8ball bot/deploy-commands.js
@@ -4,3 +4,4 @@ const { Routes } = require('discord-api-types/v9');
 const { token } = require('./data/token.json');
 const { client_id } = require('./data/client_id.json');
 const { guild_id } = require('./data/guild_id.json');
+const commands = [];
