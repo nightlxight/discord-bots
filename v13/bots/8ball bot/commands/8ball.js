825056eb2970dd8a80d03e05df95d739bcfdb7c7
nightlxight <itsbeadj@gmail.com>
2021-09-14 16:25:47 +0300

    Update 8ball.js

--- v13/bots/8ball bot/commands/8ball.js
+++ v13/bots/8ball bot/commands/8ball.js
@@ -4,5 +4,5 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('8ball')
         .setDescription('8ball command!'),
-    async execute(interaction)
+    async execute(interaction) {}
 };
