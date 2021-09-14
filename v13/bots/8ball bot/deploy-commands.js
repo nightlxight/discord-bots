9f3582b2529d742310413a42e7fd5a076b67541e
nightlxight <itsbeadj@gmail.com>
2021-09-14 16:07:54 +0300

    Update deploy-commands.js

--- v13/bots/8ball bot/deploy-commands.js
+++ v13/bots/8ball bot/deploy-commands.js
@@ -8,5 +8,5 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    
+    const command = require(`./commands/${file}`);
 }
