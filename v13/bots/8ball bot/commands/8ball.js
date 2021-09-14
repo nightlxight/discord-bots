15f9c0aac0cfbe6de32ef337b4f2b3d09e8eb047
nightlxight <itsbeadj@gmail.com>
2021-09-14 16:25:29 +0300

    Update 8ball.js

--- v13/bots/8ball bot/commands/8ball.js
+++ v13/bots/8ball bot/commands/8ball.js
@@ -2,4 +2,6 @@ var fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
+        .setName('8ball')
+        .setDescription('8ball command!')
 };
