e9596f52893622f38c59c4e364ff2d065eb1904b
nightlxight <itsbeadj@gmail.com>
2021-09-16 08:08:28 +0300

    Update index.js

--- v13/bots/8ball bot/index.js
+++ v13/bots/8ball bot/index.js
@@ -19,7 +19,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     console.log();
-    client.user.setStatus('')
+    client.user.setStatus('idle')
 });
 
 client.on('interactionCreate', async interaction => {
