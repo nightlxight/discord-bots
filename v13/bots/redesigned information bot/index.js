a41c68a88819b6f210e8ec00020a4773d7ec2116
nightlxight <itsbeadj@gmail.com>
2021-09-17 16:28:08 +0300

    Update index.js

--- v13/bots/redesigned information bot/index.js
+++ v13/bots/redesigned information bot/index.js
@@ -5,3 +5,4 @@ coming soon
 var fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const yaml = require('js-yaml');
+const settings = yaml.load(fs.readFileSync('./settings.yml', 'utf8'));
