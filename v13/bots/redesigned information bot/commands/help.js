ad8478813d7fdc69756f8c2bc4e973a44d056e70
nightlxight <itsbeadj@gmail.com>
2021-09-17 18:15:11 +0300

    Update help.js

--- v13/bots/redesigned information bot/commands/help.js
+++ v13/bots/redesigned information bot/commands/help.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const settings = yaml.load(fs.readFileSync('../settings.yml', 'utf8'));
+const yaml = require('js-yaml');
+const settings = yaml.load(fs.readFileSync('./settings.yml', 'utf8'));
 
 module.exports = {
     data: new SlashCommandBuilder()
