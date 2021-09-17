9b995a6b039fcd48ebc50c0307b02cbb37719368
nightlxight <itsbeadj@gmail.com>
2021-09-17 18:17:53 +0300

    Update user.js

--- v13/bots/redesigned information bot/commands/user.js
+++ v13/bots/redesigned information bot/commands/user.js
@@ -2,3 +2,4 @@ var fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const yaml = require('js-yaml');
+const settings = yaml.load(fs.readFileSync('./settings.yml', 'utf8'));
