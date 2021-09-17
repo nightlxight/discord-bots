7d992c130b2b602fa6ed3f66eadce44445e16c15
nightlxight <itsbeadj@gmail.com>
2021-09-17 17:19:50 +0300

    Update index.js

--- v13/bots/redesigned information bot/index.js
+++ v13/bots/redesigned information bot/index.js
@@ -23,6 +23,11 @@ client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
     const command = client.commands.get(interaction.commandName);
     if (!command) return;
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        
+    }
 })
 
 client.login(settings.token);
