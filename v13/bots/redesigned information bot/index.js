4c1a2f2ad08c14903fff08cc73901280285e4c0a
nightlxight <itsbeadj@gmail.com>
2021-09-17 17:15:35 +0300

    update redesigned bot

--- v13/bots/redesigned information bot/index.js
+++ v13/bots/redesigned information bot/index.js
@@ -6,3 +6,7 @@ var fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const yaml = require('js-yaml');
 const settings = yaml.load(fs.readFileSync('./settings.yml', 'utf8'));
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+client.login(settings.token);
