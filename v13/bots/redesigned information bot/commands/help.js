d76ba84b9058f30e33923c006cfb131885836aa5
nightlxight <itsbeadj@gmail.com>
2021-09-17 17:29:54 +0300

    Update help.js

--- v13/bots/redesigned information bot/commands/help.js
+++ v13/bots/redesigned information bot/commands/help.js
@@ -3,3 +3,36 @@
 coming soon
 
 */
+
+var fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Help command that shows list of commands'),
+    async execute(interaction) {
+        let titles = [
+            "Help command",
+            "help",
+            "🤖" //,
+            // add more titles here
+            // the last title should not contain a comma
+        ];
+
+        let title = titles[Math.floor(Math.random() * titles.length)];
+        let embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle(title)
+            .setDescription('Commands can be found below')
+            .addFields(
+                { name: `/invite`, value: 'Sends an invite link to bot!', inline: true},
+                { name: `/server`, value: 'Sends information about this server!', inline: false},
+                { name: `/user`, value: "Sends information about yourself!", inline: false},
+            )
+            .setTimestamp()
+            .setFooter('Command executed by' + " " +`${interaction.user.tag}`);
+        return interaction.reply({embeds: [embed] });
+    },
+};
