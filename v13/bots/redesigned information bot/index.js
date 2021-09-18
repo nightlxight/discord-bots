928eb7ea082f0cebaac43a476bd5ef4394625e86
nightlxight <itsbeadj@gmail.com>
2021-09-18 15:58:42 +0300

    update redesigned information bot v13

--- v13/bots/redesigned information bot/index.js
+++ v13/bots/redesigned information bot/index.js
@@ -13,7 +13,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     console.log(settings.consoleMessage);
-    console.log(settings.loggedInAs + " " + `${client.user.tag}`);
+    console.log(settings.loggedInAs + settings.space + `${client.user.tag}`);
 });
 
 client.on('interactionCreate', async interaction => {
