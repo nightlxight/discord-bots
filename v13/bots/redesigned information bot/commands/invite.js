884f514b29c96ec936310cf1b40e727400c8a17f
nightlxight <itsbeadj@gmail.com>
2021-09-18 17:05:35 +0300

    Update invite.js

--- v13/bots/redesigned information bot/commands/invite.js
+++ v13/bots/redesigned information bot/commands/invite.js
@@ -3,5 +3,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    
+    data: new SlashCommandBuilder()
+        .setName('invite')
+        .setDescription('Sends an invite link to your bot'),
+    async execute(interaction) {
+        
+    }
 }
