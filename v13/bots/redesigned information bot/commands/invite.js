4c162afdf6b299a4654b28c4e298945113758ce3
nightlxight <itsbeadj@gmail.com>
2021-09-18 17:11:12 +0300

    Update invite.js

--- v13/bots/redesigned information bot/commands/invite.js
+++ v13/bots/redesigned information bot/commands/invite.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const { random_color, red, green, aqua, cyan, lightcyan, yellow, orange, pink, purple } = require('../colors.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
