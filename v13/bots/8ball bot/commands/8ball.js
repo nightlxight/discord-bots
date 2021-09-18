0cd133f526e7e28e3d54d2146d98b24358537cad
nightlxight <itsbeadj@gmail.com>
2021-09-18 11:54:06 +0300

    Update 8ball.js

--- v13/bots/8ball bot/commands/8ball.js
+++ v13/bots/8ball bot/commands/8ball.js
@@ -18,6 +18,6 @@ module.exports = {
             .setTitle('8ball')
             .setDescription()
             .setTimestamp();
-        interaction.reply({ embeds: [embed] });
+        return interaction.reply({ embeds: [embed] });
     },
 };
