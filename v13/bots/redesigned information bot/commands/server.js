7ee7169f0fffed405b0648feb25d20c9be9fc0f7
nightlxight <itsbeadj@gmail.com>
2021-09-18 15:51:22 +0300

    Update server.js

--- v13/bots/redesigned information bot/commands/server.js
+++ v13/bots/redesigned information bot/commands/server.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const yaml = require('js-yaml');
+const settings = yaml.load(fs.readFileSync('./settings.yml', 'utf8'));
 
 
 module.exports = {
