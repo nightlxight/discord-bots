339fe2156e987be9e0ffe7fda54f7b3156b6eced
nightlxight <itsbeadj@gmail.com>
2021-09-18 11:53:39 +0300

    Update 8ball.js

--- v13/bots/8ball bot/commands/8ball.js
+++ v13/bots/8ball bot/commands/8ball.js
@@ -6,6 +6,7 @@ coming soon
 var fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const { red, green, cyan, pink, lightcyan, random_color, yellow, orange, purple, aqua } = require('../colors.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
